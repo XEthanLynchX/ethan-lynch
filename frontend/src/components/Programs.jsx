999ac46184607d4242e8eeb3ef78716604eb3a05
Ethan Lynch <ethanlynch72@gmail.com>
2024-01-18 00:06:50 -0500

    list of inperson what you get done

--- frontend/src/components/Programs.jsx
+++ frontend/src/components/Programs.jsx
@@ -47,42 +47,54 @@ const Programs = () => {
             </div>
           </div>
 
-      <div className="border-2 border-black rounded shadow-md bg-berige flex flex-col md:flex-row items-center border-t-2 mt-20 text-black shadow-2xl ">
-        <div className="w-full  md:w-1/2 p-4 border-b-2  border-black md:border-b-0">
-          <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center mt-8">Private 1:1 Coaching</h3>
-           <ul className="custom-bullet list-inside text-2xl text-black leading-relaxed m-4 font-[merriweahter] font-semibold">
-            <li>Choose from flexible durations, whether you opt for a 6, 12, or comprehensive 52-week program</li>
-            <li>Benefit from dedicated time with me serving as your personal dietitian throughout the selected program duration </li>
-            <li>Enjoy the convenience of personalized food plans tailored to your dietary preferences, lifestyle, and fitness goals</li>
-            <li>Around the clock contact for any questions and/or concerns </li>
-          </ul>
-        </div>
+        <div className="flex flex-col items-center mt-20 text-black shadow-2xl bg-berige">
+          <div className="w-full  md:w-1/2 p-4 border-b-2  border-black md:border-b-0 shadow-2xl">
+            <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center mt-8 mb-12 ">Coaching Services</h3>
+            <h4 className= "text-4xl font-bold mb-4 font-[Merriweather] text-left mt-8  ">1:1 In Person Coaching</h4>
+            <p className="text-xl font-bold font-[Merriweather] text-red  "> Current Status: Open</p>
+            <h4 className= "text-4xl font-bold mb-4 font-[Merriweather] text-left mt-8  "> What's Included: ✔️ </h4>
+              <ul className="custom-bullet list-inside leading-relaxed m-4 font-[merriweahter] text-xl ">
+                <li><span className='font-bold'>High-Level Coaching by Ethan Lynch: </span> Personalized coaching that encompasses everything you need. Tailored to your unique needs, this one-on-one service employs a science-based approach, coupled with my expertise and meticulous programming, ensuring optimal results efficiently. </li>
+                <li><span className='font-bold'>Preparation Information Package: </span> A comprehensive and personalized information package that includes essential pre-training details and vital nutrition information. It's designed to kickstart your journey with detailed and individualized insights. </li>
+                <li><span className='font-bold'>Communication Via Text / Email: </span> Get direct access to me for personalized support and quick responses to any questions you have, guaranteed within 48 hours. Your journey is my priority. </li>
+                <li><span className='font-bold'>Essiential Apps: </span> Phone applications to track workout performance and nutrional information needed for personalized diets which will be used to track progression over time. </li>
+                <li><span className='font-bold'>Individualized Training Programming: </span> Tailored to your individual needs, the training program addresses specific areas like lagging muscle groups and accommodates your availability and work / school schedule. This personalized approach ensures you achieve your desired physique efficiently. </li>
+                <li><span className='font-bold'>Individualized Nutrition Programming: </span> I'll equip you with essential tools, including a personalized Meal Plan, Grocery List, and Macronutrient Targets, ensuring you have everything you need to achieve your fitness goals. </li>
+                <li><span className='font-bold'>Supplement Recommendations </span> I'll provide you with a list of recommended supplements that will help you achieve your goals. </li>
+                <li><span className='font-bold'>Form Technique Analysis and Monitoring: </span> I'll assess and monitor your exercise technique (form) throughout our sessions, ensuring you perform movements optimally and safely for the best results and intended outcomes. </li>
+                <li><span className='font-bold'>Ongoing accountability:</span> I'll equip you with the necessary tools to stay motivated and on track. The process will be enjoyable and easily adaptable to your schedule, ensuring consistent adherence.  </li>
+                <li><span className='font-bold'>Weekly Check-In </span> Weekly check-ins are a crucial aspect of my coaching, ensuring thorough communication and progress assessment. My carefully designed protocol covers all essential aspects, guaranteeing continuous guidance toward your desired fitness goals. </li>
+              </ul>
+          
+              <div className="mt-8">
+                <h4 className="text-4xl font-bold mb-4 font-[Merriweather] text-center">Pricing</h4>
+                <p className="text-2xl font-[Merriweather] text-center">Our professional coaching services are valued at $35 per session.</p>
+              </div>
+          </div>
 
-        <div className="w-full  md:w-1/2 p-4 border-b-6 md:border-8 border-black md:border-b-0 md:border-t-0 border-b-2 ">
-          <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center  ">Custom Workout Plan</h3>
-          <ul className="custom-bullet list-inside text-2xl  leading-relaxed m-4 font-[merriweahter] font-semibold text-black">
-            <li>Optimized Workouts for the kind of Athlete you are / want to become</li>
-            <li>Custom Workouts to fit your schedule and personal needs</li>
-            <li>Introduces variety to keep workouts interesting, preventing boredom and monotony</li>
-            <li>Adapt the routine to accommodate any physical limitations or health considerations you may have</li>
-            <li>Learn how to perform exercises correctly / Safely </li>
-            
-            
-          </ul>
-        </div>
+          <div className="w-full  md:w-1/2 p-4 border-b-2  border-black md:border-b-0 shadow-2xl">
+            <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center mt-8 mb-8 ">Custom Workout Plan</h3>
+            <ul className="custom-bullet list-inside text-2xl text-black leading-relaxed m-4 font-[merriweahter] font-semibold ">
+                <li>Optimized Workouts for the kind of Athlete you are / want to become</li>
+                <li>Custom Workouts to fit your schedule and personal needs</li>
+                <li>Introduces variety to keep workouts interesting, preventing boredom and monotony</li>
+                <li>Adapt the routine to accommodate any physical limitations or health considerations you may have</li>
+                <li>Learn how to perform exercises correctly / Safely </li>
+                </ul>
+          </div>
 
-        <div className="w-full  md:w-1/2 p-4 border-b-2  border-black md:border-b-0 shadow-2xl">
-          <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center mt-8 mb-8 ">Nutrition Plan</h3>
-          <ul className="custom-bullet list-inside text-2xl text-black leading-relaxed m-4 font-[merriweahter] font-semibold ">
-            <li>Personalized nutrition plan tailored to your specific goals, needs, and lifestyle</li>
-            <li>Customized meal plan designed to help you achieve your goals</li>
-            <li>Learn how to eat healthy and make the right food choices</li>
-            <li>Learn how to read food labels and understand nutrition facts</li>
-            <li>Learn how to make healthy food choices when eating out</li>
-          </ul>
+          <div className="w-full  md:w-1/2 p-4 border-b-2  border-black md:border-b-0 shadow-2xl">
+            <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center mt-8 mb-8 ">Nutrition Plan</h3>
+            <ul className="custom-bullet list-inside text-2xl text-black leading-relaxed m-4 font-[merriweahter] font-semibold ">
+              <li>Personalized nutrition plan tailored to your specific goals, needs, and lifestyle</li>
+              <li>Customized meal plan designed to help you achieve your goals</li>
+              <li>Learn how to eat healthy and make the right food choices</li>
+              <li>Learn how to read food labels and understand nutrition facts</li>
+              <li>Learn how to make healthy food choices when eating out</li>
+            </ul>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
