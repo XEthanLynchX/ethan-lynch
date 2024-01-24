2561b2fb6492b2ad9fb4fe0774f71b87aca232fd
Ethan Lynch <ethanlynch72@gmail.com>
2024-01-23 22:33:16 -0500

    training section done

--- frontend/src/components/Programs.jsx
+++ frontend/src/components/Programs.jsx
@@ -3,11 +3,13 @@ import '../styling/style.css'
 import Program1 from '../imgs/Program1.webp';
 import Program2 from '../imgs/Program2.jpg';
 import Program3 from '../imgs/Program3.jpg';
+import Program4 from '../imgs/Program4.png';
+import Program5 from '../imgs/Program5.png';
 
 const Programs = () => {
   return (
       <div className="p-8 bg-berige text-" id="programs">
-          <h2 className="text-8xl font-bold text-black text-left mb-20 bg-berige" >Our Programs</h2>
+          <h2 className="text-8xl font-bold text-black text-left mb-20 bg-berige" > Programs</h2>
           <div className="flex flex-wrap justify-around bg-berige">
             <div className="w-full md:w-1/3 p-4 mt-20 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">
               <div className="border-0 border-black rounded p-4 shadow-2xl bg-berige ">
@@ -16,20 +18,28 @@ const Programs = () => {
                 <img src={Program2} alt="Private 1:1 Coaching" className="object-cover w-full h-96 border-8 border-black rounded-lg" />
                 <p className="text-xl mt-4 font-[Georgia]">Experience like never before with 1:1 training, where every session is crafted uniquely for you, ensuring efficient progress tailored to your specific goals and needs. Available in-person or virtually. </p>
                 <div className='flex justify-center space-x-4'>
-                 <button href="#Private" className="bg-gray  text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Learn More</button>
-                  <button href="#Private" className="bg-lightred  text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Enquire</button>
+                  <a href="#coaching" className="bg-gray text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2">
+                    Learn More
+                  </a>
+                  <a href="#contact" className="bg-lightred text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2">
+                    Inquire
+                  </a>
                 </div>
               </div>
             </div>
             <div className="w-full md:w-1/3 p-4 mt-20 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-              <div className="border-0 border-black rounded p-4 shadow-2xl bg-berige ">
-                <h3 className="text-3xl font-bold mb-4 font-[Merriweather]">Custom Workout Routine</h3>
+              <div className="border-0 border-black rounded p-4 shadow-2xl bg-berige " > 
+                <h3 className="text-3xl font-bold mb-4 font-[Merriweather]" >Custom Workout Routine</h3>
                 <h6 className="italic mb-4 -mt-4 font-semibold font-[Georgia] ">Available WorldWide</h6>         
                 <img src={Program1} alt="Custom Workout Routine" className="object-cover w-full h-96 border-8 border-black rounded-lg" />
                 <p className="text-xl mt-4 font-[Georgia]" > Discover the impact of a personalized workout plan. Through my tailored approach, we'll create a fitness routine that matches your goals, fits your schedule, and keeps you motivated on the path to a healthier, stronger you.</p>
                 <div className='flex justify-center space-x-4'>
-                  <button href="#Private" className="bg-gray  text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Learn More</button>
-                  <button href="#Private" className="bg-lightred  text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Enquire</button>
+                  <a href="#routine" className="bg-gray text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2">
+                    Learn More
+                  </a>
+                  <a href="#contact" className="bg-lightred text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2">
+                    Inquire
+                  </a>
                 </div>
               </div>
             </div>
@@ -40,16 +50,20 @@ const Programs = () => {
                 <img src={Program3} alt="Nutrition Advice / Plan" className="object-cover w-full h-96 border-8 border-black rounded-lg" />
                 <p  className="text-xl mt-4 font-[Georgia]s">Explore the benefits of a customized nutrition plan. With a designed dietary strategy that aligns with your goals, adapts to your lifestyle, and guides you towards a healthier and happier life.</p>
                 <div className='flex justify-center space-x-4'>
-                 <button href="#Private" className="bg-gray  text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Learn More</button>
-                  <button href="#Private" className="bg-lightred  text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Enquire</button>
+                  <a href="#diet" className="bg-gray text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2">
+                    Learn More
+                  </a>
+                  <a href="#contact" className="bg-lightred text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2">
+                    Inquire
+                  </a>
                 </div>
               </div>
             </div>
           </div>
 
-        <div className="flex flex-col items-center mt-20 text-black shadow-2xl bg-berige">
-          <div className="w-full  md:w-1/2 p-4 border-b-2  border-black md:border-b-0 shadow-2xl transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-10">
-            <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center mt-8 mb-12 ">Coaching Services</h3>
+        <div className="flex flex-col items-center mt-20 text-black shadow-2xl bg-berige"  id='coaching'>
+          <div className="w-full  md:w-1/2 p-4 border-b-2  border-black md:border-b-0 shadow-2xl transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-10 mb-16">
+            <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center mt-8 mb-12 " >Coaching Services</h3>
             <h4 className= "text-4xl font-bold mb-4 font-[Merriweather] text-left mt-8  ">1:1 In Person Coaching</h4>
             <p className="text-xl font-bold font-[Merriweather] text-red  "> Current Status: Open</p>
             <h4 className= "text-4xl font-bold mb-4 font-[Merriweather] text-left mt-8  "> What's Included: ✔️ </h4>
@@ -66,9 +80,14 @@ const Programs = () => {
                 <li><span className='font-bold'>Weekly Check-In </span> Weekly check-ins are a crucial aspect of my coaching, ensuring thorough communication and progress assessment. My carefully designed protocol covers all essential aspects, guaranteeing continuous guidance toward your desired fitness goals. </li>
               </ul>
 
+              <a href="#results" className="bg-red text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2 ml-6">
+                    See Results
+                  </a>
               
 
-              <h4 className= "text-4xl font-bold mb-4 font-[Merriweather] text-left mt-8  ">1:1 Virtual Coaching</h4>
+              
+
+              <h2 className= "text-5xl font-bold mb-4 font-[Merriweather] text-left mt-8  ">1:1 Virtual Coaching</h2>
               <p className="text-xl font-bold font-[Merriweather] text-red  "> Current Status: Open</p>
               <h4 className= "text-4xl font-bold mb-4 font-[Merriweather] text-left mt-8  "> What's Included: ✔️ </h4>
 
@@ -85,44 +104,76 @@ const Programs = () => {
                 <li><span className='font-bold'>Weekly Check-In </span> Weekly check-ins are a crucial aspect of my coaching, ensuring thorough communication and progress assessment. My carefully designed protocol covers all essential aspects, guaranteeing continuous guidance toward your desired fitness goals. </li>
               </ul>
 
-              <div className="mt-8 justify-between transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-40">
-  <div className="flex flex-col md:flex-row justify-between">
-    <div className="bg-gray p-4 shadow-lg transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mb-4 md:mb-0">
-      <h4 className="text-4xl font-bold font-[Merriweather] text-white">Virtual Coaching</h4>
-      <h4 className="text-xl -mt-2 font-bold font-[Merriweather] text-red ml-1">Recurring</h4>
-      <h4 className="text-2xl mt-2 font-bold font-[Merriweather] text-white">$150 Per Month</h4>
-      <p className="text-xl -mt2 font-bold font-[Merriweather] text-red ml-1"></p>
-      <div className="flex justify-between md:justify-start">
-        <button href="#Private" className="bg-lightred text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Purchase</button>
-        <button href="#Private" className="bg-lightred ml-4 md:ml-16 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Inquire</button>
-      </div>
-    </div>
+              <a href="#results" className="bg-red text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2 ml-6">
+                    See Results
+                  </a>
 
-    <div className="bg-gray p-4 shadow-lg transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-      <h4 className="text-4xl font-bold font-[Merriweather] text-white">Personal Training</h4>
-      <h4 className="text-xl -mt-2 font-bold font-[Merriweather] text-red">Recurring</h4>
-      <h4 className="text-2xl mt-2 font-bold font-[Merriweather] text-white">$175 Per Month</h4>
-      <div className="flex justify-between md:justify-start">
-        <button href="#Private" className="bg-lightred text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Purchase</button>
-        <button href="#Private" className="bg-lightred ml-4 md:ml-16 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Inquire</button>
-      </div>
-    </div>
-  </div>
-</div>
+              
+
+            <div className="mt-8 justify-between transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-40">
+              <div className="flex flex-col md:flex-row justify-between">
+                <div className="bg-gray p-4 shadow-lg transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mb-4 md:mb-0">
+                  <h4 className="text-4xl font-bold font-[Merriweather] text-white">Virtual Coaching</h4>
+                  <h4 className="text-xl -mt-2 font-bold font-[Merriweather] text-red ml-1">Recurring</h4>
+                  <h4 className="text-2xl mt-2 font-bold font-[Merriweather] text-white">$150 Per Month</h4>
+                  <p className="text-xl -mt2 font-bold font-[Merriweather] text-red ml-1"></p>
+                  <div className="flex justify-between md:justify-start">
+                    <button href="#Private" className="bg-lightred text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Purchase</button>
+                    <button href="#Private" className="bg-lightred ml-4 md:ml-16 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Inquire</button>
+                  </div>
+                </div>
+
+                <div className="bg-gray p-4 shadow-lg transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+                  <h4 className="text-4xl font-bold font-[Merriweather] text-white">Personal Training</h4>
+                  <h4 className="text-xl -mt-2 font-bold font-[Merriweather] text-red">Recurring</h4>
+                  <h4 className="text-2xl mt-2 font-bold font-[Merriweather] text-white">$200 Per Month</h4>
+                  <div className="flex justify-between md:justify-start">
+                    <button href="#Private" className="bg-lightred text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Purchase</button>
+                    <button href="#Private" className="bg-lightred ml-4 md:ml-16 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 mt-2"> Inquire</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className="w-full  md:w-1/2 p-4 border-b-2  border-black md:border-b-0 shadow-2xl">
-            <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center mt-8 mb-8 ">Custom Workout Plan</h3>
-            <ul className="custom-bullet list-inside text-2xl text-black leading-relaxed m-4 font-[merriweahter] font-semibold ">
-                <li>Optimized Workouts for the kind of Athlete you are / want to become</li>
-                <li>Custom Workouts to fit your schedule and personal needs</li>
-                <li>Introduces variety to keep workouts interesting, preventing boredom and monotony</li>
-                <li>Adapt the routine to accommodate any physical limitations or health considerations you may have</li>
-                <li>Learn how to perform exercises correctly / Safely </li>
-                </ul>
+          <div className="w-full md:w-1/2 p-4 border-b-2 border-black md:border-b-0 shadow-2xl mt-16 mb-16" id='routine'>
+            <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center mt-8 mb-8">Training Programs</h3>
+            
+            <div className="flex justify-between mb-8">
+              <div className="w-1/2 p-2 bg-gray-200 relative flex flex-col">
+                <img src={Program5} alt="Workout" className="w-full h-full" />
+                <div className='bg-dark'>
+                  <h2 className="text-3xl text-white leading-relaxed font-[merriweahter] font-bold">Optimal Bros</h2>
+                  <p className=" text-xl text-white leading-relaxed font-[merriweahter]  ">This program prioritizes hypertrophy, aiming for balanced muscle development. It involves continuous programming tailored to optimize training using a science-based approach.</p>
+                  
+                  </div>
+                
+                <button href="#Private" className="bg-lightred text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-105 mt-2"> Purchase</button>
+              </div>
+
+              <div className="w-1/2 p-2 bg-gray-200 relative flex flex-col">
+                <img src={Program4} alt="Workout" className="w-full h-full" />
+                <div className='bg-dark'>
+                  <h2 className="text-3xl text-white leading-relaxed font-[merriweahter] font-bold">Optimal Gals</h2>
+                  <p className="text-xl text-white leading-relaxed font-[merriweahter] ">
+                This program places particular emphasis on lower body and back development, targeting the glutes, lats, and delts to achieve a well-rounded V-taper, especially beneficial for women.</p>
+                </div>
+                
+                <button href="#Private" className="bg-lightred text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-105 mt-2"> Purchase</button>
+              </div>
+            </div>
+
+            <ul className="custom-bullet list-inside text-2xl text-black leading-relaxed mt-8 font-[merriweahter] font-semibold">
+            <h4 className= "text-4xl font-bold mb-4 font-[Merriweather] text-left mt-8  "> What's Included: ✔️ </h4>
+              <li>Optimized Workouts for the kind of Athlete you are / want to become</li>
+              <li>Custom Workouts to fit your schedule and personal needs</li>
+              <li>Introduces variety to keep workouts interesting, preventing boredom and monotony</li>
+              <li>Adapt the routine to accommodate any physical limitations or health considerations you may have</li>
+              <li>Learn how to perform exercises correctly / Safely </li>
+            </ul>
           </div>
 
-          <div className="w-full  md:w-1/2 p-4 border-b-2  border-black md:border-b-0 shadow-2xl">
+          <div className="w-full  md:w-1/2 p-4 border-b-2  border-black md:border-b-0 shadow-2xl mt-16" id='diet'>
             <h3 className="text-6xl font-bold mb-4 font-[Merriweather] text-center mt-8 mb-8 ">Nutrition Plan</h3>
             <ul className="custom-bullet list-inside text-2xl text-black leading-relaxed m-4 font-[merriweahter] font-semibold ">
               <li>Personalized nutrition plan tailored to your specific goals, needs, and lifestyle</li>
