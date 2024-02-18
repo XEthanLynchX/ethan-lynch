586677e1a843bc4c90513beefa3a4f1ad05f90cd
Ethan Lynch <ethanlynch72@gmail.com>
2024-02-18 18:57:13 -0500

    reroute form cancel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './views/Homepage'; // adjust the path if needed
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route></Route>
+        <Route path="/" element={<Homepage />} /> 
+        <Route path="/cancel=true" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
   );
