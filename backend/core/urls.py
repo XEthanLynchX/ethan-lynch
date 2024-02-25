ebfd70d4b61bd159f721cb8d5fc32c3244489117
Ethan Lynch <ethanlynch72@gmail.com>
2024-02-25 17:17:05 -0500

    urls update

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from user_checkout.contact_views import send_email, get_csrf_token
 
 urlpatterns = [
-    path('api/stripe/', include('user_checkout.urls')),
-    path('api/send-email/', send_email, name='send-email'),
+    path('https:/ethan-lynch-coaching-415103/api/stripe/', include('user_checkout.urls')),
+    path('https:/ethan-lynch-coaching-415103/api/send-email/', send_email, name='send-email'),
 ]
